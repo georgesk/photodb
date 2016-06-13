87bcef3dfd35b98ec32f2ce09781d67fbf405d65
georgesk <georgesk@debian.org>
2016-06-13 11:43:58 +0200

    utilitaire pour ajouter des données

---
+++ add2db.php
@@ -0,0 +1,30 @@
+<?php
+/// ajoute des données à la base de données
+$uploaddir = '/var/lib/photodb/db/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+echo '<pre>';
+$ok=copy($_FILES['userfile']['tmp_name'], $uploadfile);
+if ($ok) {
+    echo "Le fichier est valide, et a été téléchargé
+           avec succès. Voici plus d'informations :\n";
+} else {
+    echo "Attaque potentielle par téléchargement de fichiers.
+          Voici plus d'informations :\n";
+}
+
+echo "Voici quelques informations de débogage :\n\$uploadfile = ";
+print_r($uploadfile);
+echo "\n\$_FILES = ";
+print_r($_FILES);
+
+echo "</pre>\n";
+
+if($ok){
+    $cmd="./csv2db.py db/".basename($_FILES['userfile']['name']);
+    echo "$cmd<br/>\n";
+    $lastline=system($cmd);
+    echo "$lastline<br/>\n";
+}
+
+?>
